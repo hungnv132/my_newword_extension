d8f9ad6724c4365e9660c3d0c1bddc2479cd4a52
hungnv132 <hungnv132@gmail.com>
2019-12-03 07:53:09 +0700

    refactor codes

--- js/background.js
+++ js/background.js
@@ -1,29 +1,29 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log('click icon');
-  });
-  chrome.contextMenus.create({
-    "id": "sampleContextMenu",
-    "title": "Sample Context Menu",
-    "contexts": ["selection"]
-  });
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log('The color is green.');
+//   });
+//   chrome.browserAction.onClicked.addListener(function (tab) {
+//     console.log('click icon');
+//   });
+//   chrome.contextMenus.create({
+//     "id": "sampleContextMenu",
+//     "title": "Sample Context Menu",
+//     "contexts": ["selection"]
+//   });
   
-  chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    console.log("something happening from the extension");
-    var data = request.data || {};
-    var linksList = document.querySelectorAll('a');
-    [].forEach.call(linksList, function(header) {
-        header.innerHTML = request.data;
-    });
-    sendResponse({data: data, success: true});
-  });
+//   chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+//     console.log("something happening from the extension");
+//     var data = request.data || {};
+//     var linksList = document.querySelectorAll('a');
+//     [].forEach.call(linksList, function(header) {
+//         header.innerHTML = request.data;
+//     });
+//     sendResponse({data: data, success: true});
+//   });
   
-});
+// });
 
 
 
