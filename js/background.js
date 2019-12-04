b5302956b42c1e8a649fe2284d9278e81a71654e
hungnv132 <hungnv132@gmail.com>
2019-12-04 15:27:11 +0700

    update context menu

--- js/background.js
+++ js/background.js
@@ -1,17 +1,33 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
+    let searchOnOxfordContext = {
         "id": "SearchOnOxford",
         "title": "Search on OxfordLearnerDictionary",
         "contexts": ["selection"]
-    });
+    }
+    let searchOnGoogleTranslateContext = {
+        "id": "SearchOnGoogleTranslate",
+        "title": "Search on Google Translate",
+        "contexts": ["selection"]
+    }
+    chrome.contextMenus.create(searchOnOxfordContext);
+    chrome.contextMenus.create(searchOnGoogleTranslateContext);
 
     chrome.contextMenus.onClicked.addListener(function (clickedData) {
-        if (clickedData.menuItemId == "SearchOnOxford" && clickedData.selectionText) {
-            let searchURL = "https://www.oxfordlearnersdictionaries.com/search/english/?q="
-            searchURL += clickedData.selectionText
-            chrome.tabs.create({ url: searchURL });
+        if (!clickedData.selectionText) return
+
+        let searchURL = ""
+        switch(clickedData.menuItemId ) {
+            case "SearchOnOxford":
+                searchURL = "https://www.oxfordlearnersdictionaries.com/search/english/?q={searchText}"
+                break;
+            case "SearchOnGoogleTranslate":
+                searchURL = "https://translate.google.com/#view=home&op=translate&sl=en&tl=vi&text={searchText}"
+                break;
+            default:
         }
+        searchURL = searchURL.replace("{searchText}", clickedData.selectionText)
+        chrome.tabs.create({ url: searchURL });
     });
 });
